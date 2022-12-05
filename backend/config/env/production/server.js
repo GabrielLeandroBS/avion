1d1a5748c7d2f3f0b6411a550eb41d70b62f9147
Gabriel Leandro <GabrielLeanbs@Outlook.com>
2022-12-05 11:16:21 -0300

    Update server.js

--- backend/config/env/production/server.js
+++ backend/config/env/production/server.js
@@ -1,6 +1,6 @@
 // Path: ./config/env/production/server.js`
 
 module.exports = ({ env }) => ({
-  url: env('https://mysterious-mesa-27395.herokuapp.com'),
+  url: env('https://avion-production.up.railway.app/'),
 });
 
