654931c8ff758df16c357b3efafea53cd96456a1
Gabriel Leandro <GabrielLeanbs@Outlook.com>
2022-12-06 06:41:18 -0300

    Update index.ts

--- src/types/page/index.ts
+++ src/types/page/index.ts
@@ -1,37 +1,3 @@
-import { highlightedProps } from '../highlighted';
-import { popularProps } from '../popular';
-
 export type PageProps = {
   page: string;
-};
-
-export type HomeProps = {
-  Highlighted: {
-    title: string;
-    products: {
-      data: [
-        {
-          attributes: {
-            price: string;
-            slug: string;
-            stripe: string;
-            title: string;
-            description: string;
-            image: {
-              data: {
-                [0]: {
-                  attributes: {
-                    url: string;
-                  };
-                };
-              };
-            };
-          };
-        }
-      ];
-    };
-    button: string;
-    location: string;
-  };
-  Popular: popularProps;
 };
