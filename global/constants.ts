4d862f3986fc5bf6bef0fcfc18837f088ed96f14
Gabriel Leandro <GabrielLeanbs@Outlook.com>
2022-12-08 00:55:23 -0300

    Update constants.ts

--- global/constants.ts
+++ global/constants.ts
@@ -1,3 +1,3 @@
 export const VITE_VERCEL_URL = import.meta.env.VITE_APP_BASE_URL
-export const REACT_APP_STRIPE_KEY = import.meta.env.VITE_APP_STRIPE_KEY
+export const VITE_STRIPE_KEY = import.meta.env.VITE_APP_STRIPE_KEY
 
